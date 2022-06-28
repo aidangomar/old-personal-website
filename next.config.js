3faaf2a088b625f972eea0eb9d68f158bb572c30
susuwatarii <aidangomar@gmail.com>
2022-06-28 16:38:07 -0700

    bug fix

--- next.config.js
+++ next.config.js
@@ -3,4 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+const isProd = process.env.NODE_ENV === 'production'
+
+// module.exports = nextConfig
+module.exports = {
+  assetPrefix: isProd ? '/susuwatarii.github.io/' : ''
+}
